09dd9901fbb0b771943bd7b4f3b91c56236ede16
Gleb Arestov <gr1b0k@gmail.com>
2014-10-07 08:47:03 +0400

    iframe data messaging

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var fs = require('fs');
-console.dir(fs.readdirSync('.'));
+var message = {
+	last_ver: 4.0,
+	package_url: 'https://arestov.github.io/su_update_iframe/packages/install-pack-test.zip'
+};
+if (window.parent) {
+	window.parent.postMessage(message, '*');
+}
